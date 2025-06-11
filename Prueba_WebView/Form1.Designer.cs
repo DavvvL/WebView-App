517d88cacaf0d07448ae645f43d421596a81b50b
dav ... <davvvl@hotmail.com>
2025-06-10 22:36:54 -0600

    Agregar archivos de proyecto.

---
+++ Prueba_WebView/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Prueba_WebView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleBar = new System.Windows.Forms.Panel();
+            this.bBuscar = new System.Windows.Forms.Button();
+            this.tBuscar = new System.Windows.Forms.TextBox();
+            this.bCerrar = new System.Windows.Forms.Button();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.titleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleBar
+            // 
+            this.titleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.titleBar.Controls.Add(this.bBuscar);
+            this.titleBar.Controls.Add(this.tBuscar);
+            this.titleBar.Controls.Add(this.bCerrar);
+            this.titleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.titleBar.Location = new System.Drawing.Point(0, 0);
+            this.titleBar.Name = "titleBar";
+            this.titleBar.Size = new System.Drawing.Size(1094, 49);
+            this.titleBar.TabIndex = 0;
+            this.titleBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.titleBar_MouseDown);
+            // 
+            // bBuscar
+            // 
+            this.bBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bBuscar.BackColor = System.Drawing.Color.White;
+            this.bBuscar.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.bBuscar.Location = new System.Drawing.Point(884, 12);
+            this.bBuscar.Name = "bBuscar";
+            this.bBuscar.Size = new System.Drawing.Size(72, 24);
+            this.bBuscar.TabIndex = 2;
+            this.bBuscar.Text = "Buscar";
+            this.bBuscar.UseVisualStyleBackColor = false;
+            this.bBuscar.Click += new System.EventHandler(this.bBuscar_Click);
+            // 
+            // tBuscar
+            // 
+            this.tBuscar.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tBuscar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.tBuscar.Location = new System.Drawing.Point(28, 12);
+            this.tBuscar.Multiline = true;
+            this.tBuscar.Name = "tBuscar";
+            this.tBuscar.Size = new System.Drawing.Size(832, 24);
+            this.tBuscar.TabIndex = 1;
+            this.tBuscar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tBuscar.WordWrap = false;
+            // 
+            // bCerrar
+            // 
+            this.bCerrar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.bCerrar.FlatAppearance.BorderSize = 0;
+            this.bCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bCerrar.Image = global::Prueba_WebView.Properties.Resources.close;
+            this.bCerrar.Location = new System.Drawing.Point(1051, 0);
+            this.bCerrar.Name = "bCerrar";
+            this.bCerrar.Size = new System.Drawing.Size(43, 49);
+            this.bCerrar.TabIndex = 0;
+            this.bCerrar.UseVisualStyleBackColor = true;
+            this.bCerrar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.bCerrar_MouseDown);
+            this.bCerrar.MouseEnter += new System.EventHandler(this.bCerrar_MouseEnter);
+            this.bCerrar.MouseLeave += new System.EventHandler(this.bCerrar_MouseLeave);
+            this.bCerrar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.bCerrar_MouseUp);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(28, 66);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(928, 551);
+            this.webView21.Source = new System.Uri("https://www.google.com/", System.UriKind.Absolute);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            this.webView21.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(973, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 93);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Boton";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1094, 642);
+            this.ControlBox = false;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.titleBar);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.titleBar.ResumeLayout(false);
+            this.titleBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel titleBar;
+        private System.Windows.Forms.Button bCerrar;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.Button bBuscar;
+        private System.Windows.Forms.TextBox tBuscar;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Prueba_WebView/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Threading;
+using Microsoft.Web.WebView2.Core;
+
+namespace Prueba_WebView
+{
+    public partial class Form1 : Form
+    {
+        private Image original;
+        private Image hover;
+        private Image press;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            bCerrar.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            bCerrar.FlatAppearance.MouseOverBackColor = Color.Transparent;
+
+
+            original = Properties.Resources.close;
+            hover = Properties.Resources.close_hover;
+            press = Properties.Resources.close_press;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HTCAPTION = 0x2;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void titleBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(this.Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+
+        private void bCerrar_MouseEnter(object sender, EventArgs e)
+        {
+            bCerrar.Image = hover;
+        }
+
+        private void bCerrar_MouseLeave(object sender, EventArgs e)
+        {
+            bCerrar.Image = original;
+        }
+
+        private void bCerrar_MouseDown(object sender, MouseEventArgs e)
+        {
+            bCerrar.Image = press;
+        }
+
+        private void bCerrar_MouseUp(object sender, MouseEventArgs e)
+        {
+            Thread.Sleep(100);
+            System.Environment.Exit(0);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_NCHITTEST = 0x84;
+            const int HTCLIENT = 1;
+            const int HTLEFT = 10;
+            const int HTRIGHT = 11;
+            const int HTTOP = 12;
+            const int HTTOPLEFT = 13;
+            const int HTTOPRIGHT = 14;
+            const int HTBOTTOM = 15;
+            const int HTBOTTOMLEFT = 16;
+            const int HTBOTTOMRIGHT = 17;
+
+            if (m.Msg == WM_NCHITTEST)
+            {
+                base.WndProc(ref m);
+
+                Point pos = PointToClient(new Point(m.LParam.ToInt32()));
+                int gripSize = 20;
+
+                if (pos.X <= gripSize && pos.Y <= gripSize)
+                    m.Result = (IntPtr)HTTOPLEFT;
+                else if (pos.X >= Width - gripSize && pos.Y <= gripSize)
+                    m.Result = (IntPtr)HTTOPRIGHT;
+                else if (pos.X <= gripSize && pos.Y >= Height - gripSize)
+                    m.Result = (IntPtr)HTBOTTOMLEFT;
+                else if (pos.X >= Width - gripSize && pos.Y >= Height - gripSize)
+                    m.Result = (IntPtr)HTBOTTOMRIGHT;
+                else if (pos.X <= gripSize)
+                    m.Result = (IntPtr)HTLEFT;
+                else if (pos.X >= Width - gripSize)
+                    m.Result = (IntPtr)HTRIGHT;
+                else if (pos.Y <= gripSize)
+                    m.Result = (IntPtr)HTTOP;
+                else if (pos.Y >= Height - gripSize)
+                    m.Result = (IntPtr)HTBOTTOM;
+                else
+                    m.Result = (IntPtr)HTCLIENT;
+
+                return;
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private void bBuscar_Click(object sender, EventArgs e)
+        {
+            if (webView21 != null && webView21.CoreWebView2 != null)
+            {
+                webView21.CoreWebView2.Navigate(tBuscar.Text);
+            }
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
