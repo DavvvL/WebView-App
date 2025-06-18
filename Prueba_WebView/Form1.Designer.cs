fff4f9c9f19b0cfffa3c54b2b52eeba54bf6d30f
dav ... <davvvl@hotmail.com>
2025-06-18 11:09:14 -0600

    Cambio de color

--- Prueba_WebView/Form1.Designer.cs
+++ Prueba_WebView/Form1.Designer.cs
@@ -128,7 +128,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1094, 642);
             this.ControlBox = false;
             this.Controls.Add(this.bLeer);
