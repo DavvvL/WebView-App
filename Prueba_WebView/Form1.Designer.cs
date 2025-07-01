7f61d5e10dc3b48060cd2272cda9374056671939
dav ... <davvvl@hotmail.com>
2025-07-01 11:28:32 -0600

    Corrección de visualización de ventana

--- Prueba_WebView/Form1.Designer.cs
+++ Prueba_WebView/Form1.Designer.cs
@@ -143,8 +143,8 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1094, 642);
             this.ControlBox = false;
